955d2e5a0c5e2d2edfbbcdec386a37c5ad8f7eb2
ThiagoVini-dev <thiagovinicius2001lima@gmail.com>
2023-07-20 15:50:31 -0300

    update ex106

--- ex016/ex03.js
+++ ex016/ex03.js
@@ -1,3 +1,17 @@
 function contar(){
-    
+    var inicio  = document.getElementById('txti');
+    var fim = document.getElementById('txtf');
+    var passo = document.getElementById('txtp');
+    var res = document.querySelector('div#res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('IMPOSSÍVEL CONTAR')
+    } else {
+        res.innerHTML = 'Contando...'
+        let ini = Number(inicio.value)
+        let fi = Number(fim.value)
+        let pas = Number(passo.value)
+        for (let c = ini; c <=fi ; c = c + pas){
+            res.innerHTML += ` ${c} `
+        }
+    }
 }
