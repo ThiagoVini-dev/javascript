4648383235fe04ac2779b8c657ede94564f0ef63
ThiagoVini-dev <thiagovinicius2001lima@gmail.com>
2023-07-20 17:09:19 -0300

    pratica de programação

--- ex016/ex017/script.js
+++ ex016/ex017/script.js
@@ -1,10 +1,18 @@
 function Tabuada(){
-    var num = document.getElementById('txtn');
-    var tab = document.getElementById('seltab');
+    let num = document.getElementById('txtn'); // incorporação de entrada do numero em var num
+    let tab = document.getElementById('seltab'); // seleção de campo de exibição da tabuada em uma var
     if (num.value.length == 0){
-    window.alert('Por favor, digite um número')
+    window.alert('Por favor, digite um número') // caso entrada vazia mensagem de erro
     } else {
-        var n = Number(num.value);
-        var item = document.createElement('')
+        let n = Number(num.value); // conversão em numero
+        let c = 1 // inicialização
+        tab.innerHTML = '' //limpeza de campo select
+        while (c <=10){
+        let item = document.createElement('option') // criar campo de visualização de resultado
+        item.text = `${n} x ${c} = ${n*c}` // exibir calculo e resultado
+        tab.appendChild(item) // exibir tabuada
+        item.value = `tab ${c}` // determinando valores para saída de dados
+        c = c +1 // incremento 
+        }
     }
 }
