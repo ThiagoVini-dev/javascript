7cdbeb8f17f29d4260644b875b268bfc1f3aaf45
ThiagoVini-dev <thiagovinicius2001lima@gmail.com>
2023-07-20 16:51:16 -0300

    treinando

---
+++ ex016/ex017/script.js
@@ -0,0 +1,10 @@
+function Tabuada(){
+    var num = document.getElementById('txtn');
+    var tab = document.getElementById('seltab');
+    if (num.value.length == 0){
+    window.alert('Por favor, digite um número')
+    } else {
+        var n = Number(num.value);
+        var item = document.createElement('')
+    }
+}
