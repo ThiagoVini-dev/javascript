62804157d320b2ccec1ac6df7a6df6e77abee0b1
ThiagoVini-dev <thiagovinicius2001lima@gmail.com>
2023-07-20 19:23:09 -0300

    vetores

---
+++ ex018.js
@@ -0,0 +1,6 @@
+//VETORES E ESTRUTURAS DE REPETIÇÃO
+// no lugar de |c <=3| também poderia colocar c <= a.length para pegar o numero exato de casas que o vetor possui
+var a = [7,4,8,3];
+for (var c = 0; c <=3 ; c = c +1){
+console.log(` posição ${c} tem valo r ${a[c]}`)
+}
