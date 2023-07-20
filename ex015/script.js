8ebfcf58572c83242a823e6f28e7a658ee662037
ThiagoVini-dev <thiagovinicius2001lima@gmail.com>
2023-07-20 13:13:48 -0300

    atualização no script

--- ex015/script.js
+++ ex015/script.js
@@ -8,14 +8,41 @@ function verificar(){
     } else {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value);
-        res.innerHTML = `idade calculada ${idade}`
+        var img = document.createElement('img');
+        img.setAttribute('id','foto')
         var genero = '';
         if (fsex[0].checked ){
             genero = 'Homem'
+            if(idade >=0 && idade <10 && genero=='Homem'){
+                //criança
+                img.setAttribute('src','menino1.png')
+            } else if(idade>=10 && idade <21 && genero=='Homem') {
+                //jovem
+                img.setAttribute('src','homemjovem1.png')
+            } else if(idade>=21 && idade <60 && genero=='Homem') {
+                //adulto
+                img.setAttribute('src','homemadulto1.png')
+            } else if (idade >60 && genero=='Homem') {
+                //idoso
+                img.setAttribute('src','homemidoso1.png')
+            }
         } else if (fsex[1].checked){
             genero = 'Mulher'
+        } if (idade >0 && idade <10 && genero=='Mulher'){
+            //criança
+            img.setAttribute('src','menina1.png')
+        } else if (idade >=10 && idade <21 && genero=='Mulher'){
+            //jovem
+            img.setAttribute('src','mulherjovem1.png')
+        } else if (idade >=21 && idade <60 && genero=='Mulher'){
+            //adulta
+            img.setAttribute('src','mulheradulta1.png')
+        } else if (idade >=60 && genero=='Mulher'){
+            //idosa
+            img.setAttribute('src','mulheridosa1.png')
         }
-        res.innerHTML += ` <br> genero ${genero}`
+        res.innerHTML += ` <br> Detectamos ${genero} de ${idade} anos`
+        res.appendChild(img)
     }
     
 }
