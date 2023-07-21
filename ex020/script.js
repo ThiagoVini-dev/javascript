52abd1b0d0a4be37620fad066a59c359ade6649b
ThiagoVini-dev <thiagovinicius2001lima@gmail.com>
2023-07-21 11:32:35 -0300

    concluindo modulo de javascript

--- ex020/script.js
+++ ex020/script.js
@@ -17,9 +17,41 @@ function inLista(n,l){// função para validar se o numero inserido já se encon
     }
 }
 function adicionar(){
-    if (isNumero(num.value) && !inLista(num.value, valores)){
-
+    if (isNumero(num.value) && !inLista(num.value, valores)){ // se o numero estiver dentro das regras estabelecidas e não
+    valores.push(Number(num.value))  // estiver na lista (como denotado pelo sinal !, que significa NÃO)
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} adicionado`
+    lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma += valores[pos]
+        
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            } if (valores[pos]< menor){
+                menor = valores[pos]
+            }
+            media = soma / tot
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor} </p>`
+        res.innerHTML += `<p>A soma dos valores é igual a ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é igual a ${media}</p>`
     }
 }
